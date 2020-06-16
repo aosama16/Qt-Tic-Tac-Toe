1fc759addf4736438b40848ca933b7bcbe73b320
Abdelrahman Osama <abdelrahman.osama93@gmail.com>
2020-06-16 19:53:05 +0200

    Added documentation to main function.
    Explained manually handling the close event for the main window.
    
    Disabled QuitOnLastWindowClosed to avoid the dialog closing after QMessageBox is closed.
    The Main window must handle the close event manually to avoid the program running after closing it.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+    // Disabled QuitOnLastWindowClosed to avoid the dialog closing after QMessageBox is closed.
     qApp->setQuitOnLastWindowClosed(false);
 
     QApplication a(argc, argv);
     TitleScreen mainScreen;
+    // The Main window must handle the close event manually to avoid the program running after closing it.
     QObject::connect(&mainScreen, &TitleScreen::exited, &a, &QApplication::quit);
     mainScreen.show();
 
